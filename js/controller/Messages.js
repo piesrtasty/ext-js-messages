7f84ff4d9a981cd163991390628e072718e6e2b6
Luke Hamilton <lukemhamilton@gmail.com>
2012-11-14 17:21:47 -0500

    added init function to Messages controller

--- js/controller/Messages.js
+++ js/controller/Messages.js
@@ -6,7 +6,37 @@ Ext.define('Messages.controller.Messages', {
 
 	extend: 'Ext.app.Controller',
 
-	views: ['MessageField', 'MessageList', 'MessageToolbar']
+	views: ['MessageField', 'MessageList', 'MessageToolbar'],
+
+	refs: [
+		{ref: 'messageList', selector: 'messageList'},
+		{ref: 'messageToolbar', selector: 'messageToolbar'},
+		{ref: 'checkAllBox', selector: 'checkAllBox'}
+	],
+
+	init: function() {
+		this.control({
+			'messageField': {
+				keyup: this.onMessageFieldKeyup
+			},
+			'messageList': {
+				messageChecked: this.onMessageChecked,
+				itemdblclick: this.onMessageDblClicked,
+				onMessageEditKeyup: this.onMessageEditKeyup,
+				messageRemoveSelected: this.onMessageRemoveSelected
+			},
+			'completeButton': {
+				click: this.onCheckAllClick
+			}
+		});
+
+		this.getMessagesStore().on({
+			scope: this,
+			update: this.onStoreDataChanged,
+			datachanged: this.onStoreDataChanged
+		});
+
+	}
 
 
 });
