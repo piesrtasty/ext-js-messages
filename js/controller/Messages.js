9d0d2ddc94b9354cdbd10216087c720a6dc006c8
Luke Hamilton <lukemhamilton@gmail.com>
2012-11-14 18:26:37 -0500

    Added onMessageFieldKeyup function

--- js/controller/Messages.js
+++ js/controller/Messages.js
@@ -36,7 +36,60 @@ Ext.define('Messages.controller.Messages', {
 			datachanged: this.onStoreDataChanged
 		});
 
+	},
+
+	onMessageFieldKeyup: function(field, event) {
+		var ENTER_KEY_CODE = 13;
+		var value = field.getValue().trim();
+		if (event.keyCode === ENTER_KEY_CODE && value !== '') {
+			var store = this.getMessagesStore();
+			store.add({label: value, checked: false});
+			field.reset();
+			store.sync();
+		}
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
