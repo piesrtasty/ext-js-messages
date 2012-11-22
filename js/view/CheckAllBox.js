3580b1ca27583957681113fa74b11fe208f6e866
Luke Hamilton <lukemhamilton@gmail.com>
2012-11-21 19:09:03 -0500

    Began flashing out checkedAllBox View

---
+++ js/view/CheckAllBox.js
@@ -0,0 +1,24 @@
+Ext.define('Messages.view.CheckAllBox', {
+	
+	extend: 'Ext.component',
+	
+	alias: 'widget.CheckAllBox',
+	
+	tpl: '<tpl if="hasContent"><input id="toggle-all" type="checkbox" <tpl if="allComplete">checked</tpl>> <label for="toggle-all">Mark all as complete</label></tpl>',
+
+	updateCheckedState: function(totalMessages, checkedMessages) {
+		this.update({
+			hasContent: !!totalMessages,
+			allComplete: checkedMessages == totalMessages
+		});
+	},
+
+	listeners: {
+		render: function (component) {
+			component.getEl().on('click', function (event, el) {
+				var checked = !!Ext.get(el).getAttribute('checked');
+			});
+		}	
+	}
+
+});
